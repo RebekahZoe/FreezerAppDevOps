574d897411feb7f6b12b865ae7eb95a57ee01bb9
RebekahZoe <58259284+RebekahZoe@users.noreply.github.com>
2020-02-03 11:39:32 +0000

    Add files via upload

---
+++ src/main/java/com/bae/persistence/domain/Freezers.java
@@ -0,0 +1,116 @@
+
+package com.bae.persistence.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "freezers")
+public class Freezers {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "freezerId")
+	private Long id;
+	@Column(unique = true)
+	private String freezerName;
+
+	@OneToMany(fetch = FetchType.EAGER, orphanRemoval = true, cascade = CascadeType.ALL)
+	@JoinColumn(name = "freezer_id")
+	private Set<Items> items = new HashSet<>();
+
+	public Freezers(String freezerName) {
+		this.freezerName = freezerName;
+	}
+
+	public Freezers(String freezerName, Long id) {
+		this.freezerName = freezerName;
+		this.id = id;
+	}
+
+	public Freezers() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFreezerName() {
+		return freezerName;
+	}
+
+	public void setFreezerName(String freezerName) {
+		this.freezerName = freezerName;
+	}
+
+	public Set<Items> getItems() {
+		return items;
+	}
+
+	public void setItems(Set<Items> items) {
+		this.items = items;
+	}
+
+	@Override
+	public String toString() {
+		return "Freezers [id=" + id + ", freezerName=" + freezerName + ", items=" + items + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((freezerName == null) ? 0 : freezerName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((items == null) ? 0 : items.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Freezers other = (Freezers) obj;
+		if (freezerName == null) {
+			if (other.freezerName != null)
+				return false;
+		} else if (!freezerName.equals(other.freezerName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (items == null) {
+			if (other.items != null)
+				return false;
+		} else if (!items.equals(other.items))
+			return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/com/bae/persistence/repo/FreezerRepo.java
@@ -0,0 +1,13 @@
+
+package com.bae.persistence.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bae.persistence.domain.Freezers;
+
+@Repository
+public interface FreezerRepo extends JpaRepository<Freezers, Long> {
+
+	Freezers findByFreezerName(String name);
+}
